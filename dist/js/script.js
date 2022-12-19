45ca80d904ab94e508990b9d40d34567fb370648
Yurii <yramaskin228@gmail.com>
2022-12-19 21:15:26 +0200

    Add contacts section & Add adaptive

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,13 @@
 const menu = document.querySelector('.menu'),
     hamburger = document.querySelector('.hamburger'),
     closeElem = document.querySelector('.menu__close');
+    overlay = document.querySelector('.menu__overlay');
+    const arrowUp = document.querySelector('.arrow-up');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
-})
+});
 
-// const counters = document.querySelectorAll('.skills__statistics-percent'),
-//       lines = document.querySelectorAll('.skills__statistics-line span');
-
-// counters.forEach( (item, i) => {
-//     lines[i].style.width = item.innerHTML;
-// });
